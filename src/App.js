2229ce945becbaf0e3b42e5b191bc517618a60b5
Tal <artishok.tal@gmail.com>
2022-07-14 19:34:24 +0300

    deploy

--- src/App.js
+++ src/App.js
@@ -1,22 +1,16 @@
-import './App.css';
-import { MainPage } from './cmps/MainPage';
-import { Login } from './cmps/Login';
+import "./App.css";
+import { MainPage } from "./cmps/MainPage";
+import { Login } from "./cmps/Login";
 import ReactDOM from "react-dom/client";
-import {
-  BrowserRouter,
-  Routes,
-  Route,
-} from "react-router-dom";
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
         <Routes>
-          <Route path="/login" element={<Login></Login>}/>
+          <Route path="/login" element={<Login></Login>} />
           <Route path="/" element={<MainPage></MainPage>} />
-
         </Routes>
       </BrowserRouter>
     </div>
