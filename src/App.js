1d5e64481aaa44a9880e9ba32e4681a2785018e7
Tal <artishok.tal@gmail.com>
2022-07-28 13:00:10 +0300

    deploy

--- src/App.js
+++ src/App.js
@@ -2,17 +2,17 @@ import "./App.css";
 import { MainPage } from "./cmps/MainPage";
 import { Login } from "./cmps/Login";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <BrowserRouter>
+      <Router>
         <Routes>
           <Route path="/login" element={<Login></Login>} />
           <Route path="/" element={<MainPage></MainPage>} />
         </Routes>
-      </BrowserRouter>
+      </Router>
     </div>
   );
 }
